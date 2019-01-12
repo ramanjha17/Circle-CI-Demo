5ccdb110acf64816146d2fb0d3faadc0dfc899e8
rjha <rjha@10.0.0.11>
2019-01-12 11:48:19 -0500

    Testing a commit to Circle CI Project

--- src/main/java/com/circleci/demojavaspring/DemoJavaSpringApplication.java
+++ src/main/java/com/circleci/demojavaspring/DemoJavaSpringApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoJavaSpringApplication {
-
+/**
+ * This is the main class to run the Sping Boot application
+ * 
+ * */
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJavaSpringApplication.class, args);
 	}
